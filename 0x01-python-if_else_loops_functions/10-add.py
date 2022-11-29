3868080bfe7cb4c5bfa01461334884e64936eb08
Wondeman112 <wondem507@gmail.com>
2022-11-29 15:23:34 -0800

    Commit

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 10-add.py
+# Wondimagegn Emiru <wondem507@gmail.com>
+
+
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 11-pow.py
+# Wondimagegn Emiru <wondem507@gmail.com>
+
+
+def pow(a, b):
+    """Return a to the power of b."""
+    return (a ** b)
